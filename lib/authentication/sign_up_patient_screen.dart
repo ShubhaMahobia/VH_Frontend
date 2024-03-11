416d9b3055c541a8146f4adbc6334936965b5125
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-03-11 20:59:36 +0530

    temp commit 1

--- lib/authentication/sign_up_patient_screen.dart
+++ lib/authentication/sign_up_patient_screen.dart
@@ -1,25 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUpScreenPatient extends StatelessWidget {
   const SignUpScreenPatient({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Email',
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
+                style: GoogleFonts.plusJakartaSans(
+                    fontSize: 18, fontWeight: FontWeight.normal),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Password',
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Text(
+                  'Sign in for the Best Experience',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.plusJakartaSans(
+                      fontSize: 28, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: TextField(
+                  style: GoogleFonts.plusJakartaSans(
+                      color: Colors.black, fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(10),
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                    hintStyle: GoogleFonts.plusJakartaSans(
+                        color: Colors.grey, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: TextField(
+                  style: GoogleFonts.plusJakartaSans(
+                      color: Colors.black, fontWeight: FontWeight.w400),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(10),
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                    hintStyle: GoogleFonts.plusJakartaSans(
+                        color: Colors.grey, fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Forgot Password?',
+                      style: GoogleFonts.plusJakartaSans(
+                          color: Colors.blue, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 50,
+                width: 320,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Sign In',
+                    style: GoogleFonts.plusJakartaSans(
+                        color: Colors.white, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
+#include <flutter_localization/flutter_localization_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FirebaseAuthPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+  FlutterLocalizationPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLocalizationPluginCApi"));
 }
