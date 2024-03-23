6fe42569e45cb55b1674b55f1156122749fd1fff
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-03-23 14:40:41 +0530

    added home_page file

---
+++ lib/patient/controller/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PatientHomePage extends StatelessWidget {
+  const PatientHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
